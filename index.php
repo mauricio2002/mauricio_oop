923a9a930d736de82191ecf9b087796556670f8c
mauricio2002 <mauricio200243@gmail.com>
2019-06-04 15:43:51 +0800

    問1作業
    作業途中。

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$kid = new Kid();
+
+class kid{
+// プロパティ
+    public $name;
+    public $sex;
+// 初期値
+    function __construct(){
+        $this->name = 'Seed';
+        $this->sex = 'boy';
+    }
+// メソッド
+    function setName($name){
+
+    }
+
+}
