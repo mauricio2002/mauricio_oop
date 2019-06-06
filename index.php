67528078c2da79c9a7b8b1be44660194cd7005cc
mauricio2002 <mauricio200243@gmail.com>
2019-06-06 08:47:36 +0800

    問一作業
    作業終了

--- index.php
+++ index.php
@@ -1,19 +1,39 @@
 <?php
-
 $kid = new Kid();
+$kid->setSex('rady');
+$kid->setName(' まるこ');
+$kid->showName();
 
-class kid{
-// プロパティ
+class Kid{
     public $name;
     public $sex;
-// 初期値
     function __construct(){
         $this->name = 'Seed';
         $this->sex = 'boy';
     }
-// メソッド
-    function setName($name){
 
+    public function setSex($seibetu){
+        $this->sex=$seibetu;
     }
 
+    public function getSex(){
+        return $this->sex;
+    }
+
+
+        public function setName($namae){
+        $this->name=$namae;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function showName(){
+        if($this->sex== 'boy'){
+                echo "$this->name".'くん';
+        }elseif($this->sex== 'rady'){
+        echo "$this->name".'ちゃん';
+    }
+}
 }
